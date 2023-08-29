d6604c8c4aa9843ee5936599ab19e1f1fd5ae5df
Andy Hoch <andreas.hoch@me.com>
2023-08-29 15:29:40 +0200

    New Sport Session Form

--- app/controllers/sport_sessions_controller.rb
+++ app/controllers/sport_sessions_controller.rb
@@ -1,5 +1,5 @@
 class SportSessionsController < ApplicationController
-  skip_before_action :authenticate_user!, only: %w[index show]
+  skip_before_action :authenticate_user!, only: %w[index show new create]
   before_action :set_sport_session, only: %w[show edit]
 
   def index
