547044eafb009ad84037dcb328aeaedc45c9370b
Andy Hoch <andreas.hoch@me.com>
2023-08-29 12:45:53 +0200

    Changes

---
+++ app/controllers/sport_sessions_controller.rb
@@ -0,0 +1,43 @@
+class SportSessionsController < ApplicationController
+  before_action :set_sport_session, only: %w[show edit]
+
+  def index
+    @sport_sessions = SportSession.all
+  end
+
+  def show
+    @full = @sport_session.max_attendees == @sport_session.attendees.count?
+  end
+
+  def new
+    @sport_session = SportSession.new
+  end
+
+  def create
+    @sport_session.new(sport_session_params)
+    @sport_session.user = current_user
+    @sport_session.chatroom = Chatroom.create
+    if @sport_session.save
+      redirect_to sport_sessions_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  private
+
+  def set_sport_session
+    @sport_session = SportSession.find(params[:id])
+  end
+
+  def sport_session_params
+    params.require(:sport_session).permit(:title, :description, :address, :start_time, :end_time, :skill_level, :max_attendees, :price, :venue_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :sport_sessions, only: %w[index show new create]
 end
