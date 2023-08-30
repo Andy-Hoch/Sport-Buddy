f01f90a4658837664e8727c43820f0d1a435e554
Andy Hoch <andreas.hoch@me.com>
2023-08-30 15:45:59 +0200

    Updates
    Deleted address from Sessions and dropped the Session Categories Table and created new relationshops and updated models

--- app/models/sport_category.rb
+++ app/models/sport_category.rb
@@ -1,4 +1,3 @@
 class SportCategory < ApplicationRecord
-  has_many :session_categories
-  has_many :sport_sessions, through: :session_categories
+  belongs_to :sport_session
 end
--- app/models/sport_session.rb
+++ app/models/sport_session.rb
@@ -1,11 +1,10 @@
 class SportSession < ApplicationRecord
   belongs_to :user
   belongs_to :venue
+  has_one :sport_category
   has_one :chatroom, dependent: :destroy
   has_many :attendees, dependent: :destroy
   has_many :participants, through: :attendees, source: :user
-  has_many :session_categories
-  has_many :sport_categories, through: :session_categories
 
   enum skill_level: %i[beginner intermediate advanced]
 end
