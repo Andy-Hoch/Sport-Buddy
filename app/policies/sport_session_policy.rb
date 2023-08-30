062a0a65fdbb74d82f20afbb6dc32d7e8341df31
Andy Hoch <andreas.hoch@me.com>
2023-08-30 12:37:22 +0200

    Sport Sessions Pundit

---
+++ app/policies/sport_session_policy.rb
@@ -0,0 +1,21 @@
+class SportSessionPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def new?
+    true
+  end
+end
