cb1a839ce2122022d18d203d8362b97c7647c189
Nicole Deutrich <nicoledeutrich@t-online.de>
2023-09-04 12:22:05 +0200

    add route, controller action and view to display 'my session'

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,10 @@ class PagesController < ApplicationController
   def home
     @sport_categories = policy_scope(SportCategory)
   end
+
+  def list
+    created_sessions = SportSession.where(user_id: current_user.id)
+    # joined_sessions = Attendee.where(user: current_user)
+    @my_sessions = created_sessions # + joined_sessions
+  end
 end
