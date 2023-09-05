62d03767c68332bc98e2685beed9be1b59cbbd85
froi dautaj <froidautaj0@gmail.com>
2023-09-05 10:51:27 +0200

    Can't get it to work

--- app/controllers/sport_sessions_controller.rb
+++ app/controllers/sport_sessions_controller.rb
@@ -9,7 +9,9 @@ class SportSessionsController < ApplicationController
     @markers = @venues.map do |venue|
       {
         lat: venue.latitude,
-        lng: venue.longitude
+        lng: venue.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: { venue: venue }),
+        marker_html: render_to_string(partial: "marker", locals: { venue: venue })
       }
     end
   end
