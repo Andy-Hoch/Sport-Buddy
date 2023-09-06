bc89baed6f534d9a0fde085de5e4d6f66b64dce5
Hywel Jones <hywelj@yahoo.com>
2023-09-06 12:50:50 +0200

    Inserted validations though more work needed

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -2,9 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr"
 
 // Connects to data-controller="flatpickr"
-export default class extends Controller {
+export default class extends Controller{
+  static targets = [ "startTime", "endTime" ]
+
   connect() {
-    flatpickr(this.element, {
+    flatpickr(this.startTimeTarget, {
+      enableTime: true,
+      dateFormat: "Y-m-d H:i",
+      time_24hr: true,
+    })
+
+    flatpickr(this.endTimeTarget, {
       enableTime: true,
       dateFormat: "Y-m-d H:i",
       time_24hr: true,
--- app/models/sport_session.rb
+++ app/models/sport_session.rb
@@ -6,5 +6,22 @@ class SportSession < ApplicationRecord
   has_many :attendees, dependent: :destroy
   has_many :participants, through: :attendees, source: :user
 
+  validate :start_time_must_be_before_end_time
+  validate :start_time_in_future
+
   enum skill_level: %i[beginner intermediate advanced]
+
+  private
+
+  def start_time_must_be_before_end_time
+    if start_time.present? && end_time.present? && start_time >= end_time
+      errors.add(:start_time, "must be before the end date")
+    end
+  end
+
+  def start_time_in_future
+    if start_time.present? && start_time <= Time.now
+      errors.add(:start_time, "must be in the future")
+    end
+  end
 end
